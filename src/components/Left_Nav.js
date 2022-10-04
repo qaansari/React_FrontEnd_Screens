662aacd8cae75b9d3bba175fcf5e663a9f95494e
khushalkhan9288@gmail.com <khushalkhan9288@gmail.com>
2022-10-04 08:25:55 -0700

    ToolTip

--- src/components/Left_Nav.js
+++ src/components/Left_Nav.js
@@ -2,35 +2,38 @@ import React from "react";
 import { Link } from "react-router-dom";
 function Left_Nav() {
   return (
-    <div class="sidenav_left">
-      <div class="nav_icon tooltip">
-        <i class="fa fa-bars"></i>
-        <span class="tooltiptext">Home</span>
+    
+    <div class="sidenav_left" >
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="Home">
+     <i class="fa fa-bars"></i>
+       
       </div>
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="BTC">
         <Link to={"/bitcoin"}>
           <i class="fa fa-btc"></i>
         </Link>
       </div>
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="NFT">
         <Link to={"/nft"}>NFT</Link>
       </div>
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="US/SAR/BTC">
         <i class="fa fa-exchange"></i>
       </div>
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="Wallets">
         <i class="fa fa-pie-chart"></i>
       </div>
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="Layout Type">
         <i class="fa fa-eye"></i>
       </div>
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="English/Arabic">
         <i class="fa fa-globe"></i>
       </div>
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="Info">
         <i class="fa fa-question-circle"></i>
       </div>
     </div>
+    
   );
+  
 }
 export default Left_Nav;
--- src/components/Right_Nav.js
+++ src/components/Right_Nav.js
@@ -1,32 +1,32 @@
 function Right_Nav({}) {
   return (
     <div class="sidenav_right">
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip"  data-placement="top"  title="On/Off">
         <i class="fa fa-power-off"></i>
       </div>
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="Trade History">
         <i class="fa fa-history"></i>
       </div>
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="WatchList">
         <i class="fa fa-th-list"></i>
       </div>
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="Chat">
         <i class="fa fa-comment"></i>
       </div>
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="News">
         <i class="fa fa-newspaper-o"></i>
       </div>
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="Alerts">
         <i class="fa fa-bell"></i>
       </div>
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="Twitter">
         <i class="fa fa-twitter"></i>
       </div>
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="YouTube">
         <i class="fa fa-youtube-play"></i>
       </div>
 
-      <div class="nav_icon">
+      <div class="nav_icon" data-toggle="tooltip" data-placement="top" title="TikTok">
         <img src="assets/img/tiktok.png" class="tiktok" />
       </div>
     </div>
